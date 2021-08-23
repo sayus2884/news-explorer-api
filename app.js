1f4c48e95a97c4ea6baf23fc0fd67e7821534c00
bakagai <>
2021-08-23 14:40:48 -0700

    feature: add centralized err handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const NotFoundError = require('./errors/not-found-err');
+
 const { PORT = 8080 } = process.env;
 
 const app = express();
@@ -9,6 +11,18 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/newsdb');
 
 app
+
+  .use('*', () => {
+    throw new NotFoundError('Requested resource not found');
+  })
+  
+  .use((err, req, res, next) => {
+    const { statusCode, message } = err;
+
+    res.status(statusCode)
+      .send({ message: statusCode === 500 ? 'An error occured on the server.' : message });
+  })
+
   .listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
   });
