17bbcc7733e14c7c4fd66b9273b1d74ff9068543
bakagai <>
2021-08-24 20:55:22 -0700

    refactor: separate rate limiter to middleware

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
 const { errors, celebrate, Joi } = require('celebrate');
 require('dotenv').config();
 
@@ -14,14 +13,11 @@ const NotFoundError = require('./errors/not-found-err');
 const routes = require('./routes');
 
 const auth = require('./middlewares/auth');
+const requestLimiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { createUser, login } = require('./controllers/users');
 
-const requestLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 50,
-});
 
 const { PORT = 8080, DB = 'mongodb://localhost:27017/dev' } = process.env;
 
